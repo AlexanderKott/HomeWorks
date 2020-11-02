e4a19fd5feadb29e6cd50648d383b8c06802a43d
AlexK <alexander.kotikov.mail@gmail.com>
2020-11-02 14:15:29 +0300

    DZ 4.2

---
+++ src/main/java/homeworks/hw42/Main.java
@@ -0,0 +1,11 @@
+package homeworks.hw42;
+
+public class Main {
+    public static void main(String[] args) {
+        Box<String, String> sample1 = new Box<>("имя", "Нетология");
+        System.out.println(sample1);
+
+        Box<Integer, Boolean> sample2 = new Box<>(1, Boolean.TRUE);
+        System.out.println(sample2);
+    }
+}
